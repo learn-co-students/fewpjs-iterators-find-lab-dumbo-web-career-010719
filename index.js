30d17d2999c804ee18986c85e0869bc7dd879f23
woodylucas <github email address>
2019-07-21 11:45:46 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (record) => {
+     let result = record.find(record => {
+       if (record.result === "W") {
+         return record.year;
+       } else {
+         return undefined;
+       }
+     })
+     return result.year;
+   }
